4ed466622a668caadeb5ec4f23cbc8dd52ba9d2e
isuifeng <847751875@qq.com>
2020-04-08 00:03:46 +0800

    单元测试

--- sort/sort_test.go
+++ sort/sort_test.go
@@ -1,14 +1,23 @@
 package sort
 
 import (
-	"fmt"
 	"testing"
 )
 
 var data = []int{5, 39, 6666, 9, 54, 777, 324}
 
+// IsSorted 判断是否为有序数组
+func IsSorted(t *testing.T, nums []int) {
+	for i := 1; i < len(nums); i++ {
+		if nums[i] <= nums[i-1] {
+			t.Error("不是有序数组")
+			return
+		}
+	}
+
+}
+
 func TestSelection(t *testing.T) {
-	fmt.Println(data)
 	Selection(data)
-	fmt.Println(data)
+	IsSorted(t, data)
 }
