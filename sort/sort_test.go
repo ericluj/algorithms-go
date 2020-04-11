c8217720db2f065ba9904c5045f4a905e946fe83
isuifeng <847751875@qq.com>
2020-04-11 17:27:54 +0800

    快速排序

--- sort/sort_test.go
+++ sort/sort_test.go
@@ -9,13 +9,13 @@ var data = []int{5, 7, 3, 2, 0, 1, 6, 4, 9, 8}
 
 // IsSorted 判断是否为有序数组
 func IsSorted(t *testing.T, nums []int) {
+	defer fmt.Println(nums)
 	for i := 1; i < len(nums); i++ {
 		if nums[i] <= nums[i-1] {
 			t.Error("不是有序数组")
 			return
 		}
 	}
-	fmt.Println(nums)
 }
 
 func TestSelection(t *testing.T) {
@@ -40,5 +40,10 @@ func TestMerge(t *testing.T) {
 
 func TestMergeBu(t *testing.T) {
 	MergeBu(data)
+	IsSorted(t, data)
+}
+
+func TestQuick(t *testing.T) {
+	Quick(data)
 	IsSorted(t, data)
 }
