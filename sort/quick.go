5f041d45df31ce7579fab32427a564da6638c856
ericluj <847751875@qq.com>
2020-04-11 22:19:04 +0800

    三向拆分快速排序

--- sort/quick.go
+++ sort/quick.go
@@ -23,12 +23,12 @@ func quickSort(nums []int, lo, hi int) {
 }
 
 func partition(nums []int, lo, hi int) int {
-
+	v := nums[lo]
 	i := lo + 1
 	j := hi
 	for {
 		//从左往右扫描，找到第一个大于等于a[lo]的元素
-		for nums[i] < nums[lo] {
+		for nums[i] < v {
 			if i == hi {
 				break
 			}
@@ -37,7 +37,7 @@ func partition(nums []int, lo, hi int) int {
 		//此时nums[i]应该是大于等于nums[lo]，或者为nums[hi](nums[hi]<nums[lo])
 
 		//从右往左扫描，找到第一个小于等于a[lo]的元素
-		for nums[j] > nums[lo] {
+		for nums[j] > v {
 			if j == lo {
 				break
 			}
@@ -54,4 +54,42 @@ func partition(nums []int, lo, hi int) int {
 	}
 	lib.Exch(nums, lo, j)
 	return j
+}
+
+// Quick3way 三向切分的快速排序
+func Quick3way(nums []int) {
+	quick3waySort(nums, 0, len(nums)-1)
+}
+
+func quick3waySort(nums []int, lo, hi int) {
+	//nums[lo,lt-1]小于v
+	//nums[gt+1,hi]大于v
+	//nums[lt,i-1]等于v
+	//nums[i,gt]未确定
+	//不断递归，缩小gt-i的值
+
+	if hi <= lo {
+		return
+	}
+
+	v := nums[lo] //不能使用nums[lo]去做比较，因为元素交换，nums[lo]的值在改变
+	lt := lo
+	i := lo + 1
+	gt := hi
+
+	for i <= gt {
+		if nums[i] < v {
+			lib.Exch(nums, i, lt) //交换位置后nums[lt]<v，nums[i]==v
+			lt++                  //lt++，此时nums[lt]==v，即上一步的nums[i]
+			i++                   //取下一个数来进行比较
+		} else if nums[i] > v {
+			lib.Exch(nums, i, gt) //交换位置后nums[gt]>v，nums[i]的值未确定，所以不需要i++
+			gt--                  //gt--，此时nums[gt]未确定
+		} else if nums[i] == v {
+			i++ //i++，取一个新的未确定是来比较
+		}
+	}
+
+	quick3waySort(nums, lo, lt-1)
+	quick3waySort(nums, gt+1, hi)
 }
