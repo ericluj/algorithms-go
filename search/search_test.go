724f674647406383268c59fcbea3a3abcf4c64db
ericluj <847751875@qq.com>
2020-04-13 23:08:58 +0800

    fix

--- search/search_test.go
+++ search/search_test.go
@@ -8,8 +8,14 @@ import (
 
 func TestBST(t *testing.T) {
 	b := NewBST()
-	for i := 0; i < 100; i++ {
+	for i := 20; i < 30; i++ {
 		b.Put(Key("key"+strconv.Itoa(i)), i)
 	}
-	fmt.Println(b.Get(Key("key" + strconv.Itoa(66))))
+	for i := 10; i < 20; i++ {
+		b.Put(Key("key"+strconv.Itoa(i)), i)
+	}
+	fmt.Println(b.Get(Key("key" + strconv.Itoa(26))))
+	fmt.Println(b.Min())
+	fmt.Println(b.Max())
+	fmt.Println(b.Floor(Key("key26")))
 }
