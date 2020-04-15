afe2cddc1f14f1ea1ea920ce27ec3a6a5b1d904d
ericluj <847751875@qq.com>
2020-04-15 22:40:21 +0800

    fix

--- search/search_test.go
+++ search/search_test.go
@@ -8,14 +8,16 @@ import (
 
 func TestBST(t *testing.T) {
 	b := NewBST()
-	for i := 20; i < 30; i++ {
+	for i := 111; i <= 120; i++ {
 		b.Put(Key("key"+strconv.Itoa(i)), i)
 	}
-	for i := 10; i < 20; i++ {
+	for i := 101; i <= 110; i++ {
 		b.Put(Key("key"+strconv.Itoa(i)), i)
 	}
-	fmt.Println(b.Get(Key("key" + strconv.Itoa(26))))
+	fmt.Println(b.Root.Size())
+	fmt.Println(b.Get(Key("key" + strconv.Itoa(116))))
 	fmt.Println(b.Min())
 	fmt.Println(b.Max())
-	fmt.Println(b.Floor(Key("key26")))
+	fmt.Println(b.Floor(Key("key126")))
+	fmt.Println(b.Select(6))
 }
