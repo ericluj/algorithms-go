50f7ce35c9578c9e6380d4f34db8e4bbdb940209
ericluj <847751875@qq.com>
2022-11-21 23:23:56 +0800

    LazyPrimMST

--- graph/lazy_prim_mst.go
+++ graph/lazy_prim_mst.go
@@ -1,6 +1,7 @@
 package graph
 
 // 最小生成树的Prim算法的延时实现
+// 切分定理：在一幅加权图中，给定任意的切分，它的横切边中权重最小者必然属于图的最小生成树
 type LazyPrimMST struct {
 	marked []bool        // 最小生成树的顶点
 	mst    *Queue[*Edge] // 最小生成树的边
@@ -13,20 +14,20 @@ func NewLazyPrimMST(g *EdgeWeightedGraph) *LazyPrimMST {
 		mst:    NewQueue[*Edge](),
 		pq:     NewMinQueue(),
 	}
-	p.visit(g, 0) //假设g是连通的
+	p.visit(g, 0) // 假设g是连通的
 
-	for !p.pq.IsEmpty() {
+	for !p.pq.IsEmpty() { // 是否还剩有效的横切边
 		// 从pq中得到权重最小的边
 		e := p.pq.DelMin()
-		// 跳过失效的边
+		// 跳过失效的边（如果横切边的两个顶点都被标记，说明已经在最小生成树中了）
 		v := e.eigher()
 		w := e.other(v)
 		if p.marked[v] && p.marked[w] {
 			continue
 		}
-		// 将边添加到树中
+		// 将边添加到最小生成树中
 		p.mst.Enqueue(e)
-		// 将顶点v或w添加到树中
+		// 将顶点v或w标记到最小生成树中
 		if !p.marked[v] {
 			p.visit(g, v)
 		}
@@ -39,7 +40,7 @@ func NewLazyPrimMST(g *EdgeWeightedGraph) *LazyPrimMST {
 }
 
 func (p *LazyPrimMST) visit(g *EdgeWeightedGraph, v int) {
-	// 标记顶点v并将所有连接v和未被标记顶点加入pq
+	// 标记顶点v并将所有连接v且未被标记顶点的边加入pq
 	p.marked[v] = true
 	for _, e := range g.Adj(v).Data() {
 		if !p.marked[e.other(v)] {
