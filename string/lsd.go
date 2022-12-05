e78404106912512cb5dfce210d7fb60c2e4b5c72
ericluj <847751875@qq.com>
2022-12-05 14:37:42 +0800

    LSD

---
+++ string/lsd.go
@@ -0,0 +1,41 @@
+package string
+
+// 低位优先的字符串排序
+type LSD struct{}
+
+func NewLSD() *LSD {
+	return &LSD{}
+}
+
+func (lsd *LSD) Sort(a []string, w int) {
+	// 通过前w个字符将a[]排序
+	N := len(a)
+	R := 256
+	aux := make([]string, N)
+
+	for d := w - 1; d >= 0; d-- {
+		// 根据第d个字符用键索引计数法排序
+
+		// 计算出现频率
+		count := make([]int, R+1)
+		for i := 0; i < N; i++ {
+			count[a[i][d]+1]++
+		}
+
+		// 将频率转换为索引
+		for i := 0; i < R; i++ {
+			count[i+1] += count[i]
+		}
+
+		// 将元素分类
+		for i := 0; i < N; i++ {
+			aux[count[a[i][d]]] = a[i]
+			count[a[i][d]]++
+		}
+
+		// 回写
+		for i := 0; i < N; i++ {
+			a[i] = aux[i]
+		}
+	}
+}
--- string/string_test.go
+++ string/string_test.go
@@ -1,1 +1,28 @@
 package string
+
+import (
+	"fmt"
+	"testing"
+)
+
+// 低位优先的字符串排序
+func TestLSD(t *testing.T) {
+	data := []string{
+		"4PGC938",
+		"2IYE230",
+		"3CI0720",
+		"1ICK750",
+		"1OHV845",
+		"4JZY524",
+		"1ICK750",
+		"3CI0720",
+		"1OHV845",
+		"1OHV845",
+		"2RLA629",
+		"2RLA629",
+		"3ATW723",
+	}
+	lsd := NewLSD()
+	lsd.Sort(data, 7)
+	fmt.Println(data)
+}
